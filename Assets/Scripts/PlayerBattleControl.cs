a6f4303d86efb6b6fa28e9f2025db3164f93740f
Geoffrey Cheung <kahogeoff@gmail.com>
2016-12-31 02:18:24 +0800

    Added camera control and basic aiming

---
+++ Assets/Scripts/PlayerBattleControl.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+[RequireComponent(typeof(Animator))]
+public class PlayerBattleControl : MonoBehaviour {
+	public GameObject Redball;
+	public Transform GunPoint;
+
+	private Animator _selfAnimator;
+	private RaycastHit _hit;
+	private Ray _cameraRay;
+	// Use this for initialization
+	void Start () {
+		_selfAnimator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (CrossPlatformInputManager.GetButtonDown ("Fire2")) {
+			_selfAnimator.SetBool ("Aiming", true);
+		} else if (CrossPlatformInputManager.GetButtonUp ("Fire2")) {
+			_selfAnimator.SetBool ("Aiming", false);
+		}
+
+		if(_selfAnimator.GetBool("Aiming")){
+			_cameraRay = new Ray(
+				Camera.main.transform.position, Camera.main.transform.forward
+			);
+			transform.LookAt (new Vector3(_cameraRay.GetPoint (10).x, 0, _cameraRay.GetPoint (10).z));
+
+			Vector3 p1 = GunPoint.position;
+			float distanceToObstacle = 0;
+
+			if (Physics.SphereCast(_cameraRay, 0.1f, out _hit, float.MaxValue)) {
+				Redball.transform.position = _hit.point;
+			}
+		}
+
+		if (_selfAnimator.GetCurrentAnimatorStateInfo (0).IsName ("RangeAttack")) {
+			transform.LookAt (new Vector3(_cameraRay.GetPoint (10).x, 0, _cameraRay.GetPoint (10).z));
+		}
+	}
+
+	void OnAnimatorIK()
+	{
+		
+		if(_selfAnimator) {
+
+			//if the IK is active, set the position and rotation directly to the goal. 
+			if(_selfAnimator.GetCurrentAnimatorStateInfo(0).IsName("RangeAttack")) {
+
+				_selfAnimator.SetIKPositionWeight (AvatarIKGoal.LeftHand, 1);
+				_selfAnimator.SetLookAtWeight (1);
+
+				_selfAnimator.SetLookAtPosition (_cameraRay.GetPoint(10));
+				_selfAnimator.SetIKPosition(AvatarIKGoal.LeftHand, _cameraRay.GetPoint(10));
+
+			}
+
+			//if the IK is not active, set the position and rotation of the hand and head back to the original position
+			else {
+				_selfAnimator.SetLookAtWeight (0);
+				_selfAnimator.SetIKPositionWeight (AvatarIKGoal.LeftHand, 0);       
+				
+			}
+		}
+	}   
+}
