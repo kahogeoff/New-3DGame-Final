aa0ba2b94f1971783edc800668a2a7baa8bfb6a6
Geoffrey Cheung <kahogeoff@gmail.com>
2017-01-07 18:37:22 +0800

    Improve the aimming

--- Assets/Scripts/PlayerBattleControl.cs
+++ Assets/Scripts/PlayerBattleControl.cs
@@ -5,7 +5,11 @@ using UnityStandardAssets.CrossPlatformInput;
 [RequireComponent(typeof(Animator))]
 public class PlayerBattleControl : MonoBehaviour {
 	public GameObject Redball;
+	public Transform RangeWeapon;
+	public GameObject BulletObject;
 	public Transform GunPoint;
+	public Transform RangeWeaponHandle;
+	public Transform LeftHandPivot;
 	public float MaximumDistance = 1000f;
 
 	private Animator _selfAnimator;
@@ -24,6 +28,7 @@ public class PlayerBattleControl : MonoBehaviour {
 			_selfAnimator.SetBool ("Aiming", false);
 		}
 
+		// In Aiming State
 		if(_selfAnimator.GetBool("Aiming")){
 			_cameraRay = new Ray(
 				Camera.main.transform.position, Camera.main.transform.forward
@@ -39,32 +44,44 @@ public class PlayerBattleControl : MonoBehaviour {
 		}
 
 		if (_selfAnimator.GetCurrentAnimatorStateInfo (0).IsName ("RangeAttack")) {
-			transform.LookAt (new Vector3(_cameraRay.GetPoint (10).x, 0, _cameraRay.GetPoint (10).z));
+			Vector3 tmp_targetPoint = new Vector3 (_cameraRay.GetPoint (10).x, 0, _cameraRay.GetPoint (10).z);
+			transform.LookAt (tmp_targetPoint);
+			//_selfAnimator.GetBoneTransform (HumanBodyBones.LeftShoulder).LookAt(Redball.transform, Vector3.up);
+			RangeWeapon.LookAt (Redball.transform.position);
+			if (CrossPlatformInputManager.GetButtonDown ("Fire1") && CrossPlatformInputManager.GetButton ("Fire1")) {
+				Instantiate (BulletObject, GunPoint.position, GunPoint.rotation);
+			}
+		} else {
+			Transform tmp_boneTransform = _selfAnimator.GetBoneTransform (HumanBodyBones.LeftHand);
+			RangeWeapon.position = LeftHandPivot.position;
+			RangeWeapon.rotation = LeftHandPivot.rotation;
 		}
 	}
 
 	void OnAnimatorIK()
 	{
 		
-		if(_selfAnimator) {
+		if (_selfAnimator) {
 
 			//if the IK is active, set the position and rotation directly to the goal. 
-			if(_selfAnimator.GetCurrentAnimatorStateInfo(0).IsName("RangeAttack")) {
+			if (_selfAnimator.GetCurrentAnimatorStateInfo (0).IsName ("RangeAttack")) {
+				_selfAnimator.SetLookAtWeight (1);
+				_selfAnimator.SetLookAtPosition (_cameraRay.GetPoint (10));
 
 				_selfAnimator.SetIKPositionWeight (AvatarIKGoal.LeftHand, 1);
-				_selfAnimator.SetLookAtWeight (1);
+				_selfAnimator.SetIKPosition (AvatarIKGoal.LeftHand, RangeWeaponHandle.position);
 
-				_selfAnimator.SetLookAtPosition (_cameraRay.GetPoint(10));
-				_selfAnimator.SetIKPosition(AvatarIKGoal.LeftHand, _cameraRay.GetPoint(MaximumDistance));
-
+				_selfAnimator.SetIKRotationWeight (AvatarIKGoal.LeftHand, 1);
+				_selfAnimator.SetIKRotation (AvatarIKGoal.LeftHand, RangeWeaponHandle.rotation);
 			}
 
 			//if the IK is not active, set the position and rotation of the hand and head back to the original position
 			else {
 				_selfAnimator.SetLookAtWeight (0);
-				_selfAnimator.SetIKPositionWeight (AvatarIKGoal.LeftHand, 0);       
-				
+				_selfAnimator.SetIKRotationWeight (AvatarIKGoal.LeftHand, 0);
+				_selfAnimator.SetIKPositionWeight (AvatarIKGoal.LeftHand, 0);  
 			}
+		} else {
 		}
 	}   
 }
