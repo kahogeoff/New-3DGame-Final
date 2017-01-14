6b438f72127c0ff0fa73c3b7e94a396478cbec22
geek2460 <kevin19930525@gmail.com>
2017-01-15 02:41:29 +0800

    Spawn Enemy in Wave mode and Wave UI

---
+++ Assets/Scripts/EnemyGenerator.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGenerator : MonoBehaviour
+{
+    public GameObject FasterEnemy;
+    public GameObject HeavyEnemy;
+    public int EnemyThresHold;
+    public int WaveNum;
+    public int EnemyNumPerWave;
+    public int EnemyNumNow;
+    public float TimeCounter;
+    private GameObject[] SpawnPointList;
+    public int WaveCounter;
+    private int SpawnPosNum;
+    private GameObject tmp_Enemy;
+    private bool firstWave;
+
+    // Use this for initialization
+    void Start()
+    {
+        SpawnPointList = GameObject.FindGameObjectsWithTag("Spawn");
+        SpawnPosNum = SpawnPointList.Length;
+        WaveCounter = 0;
+        TimeCounter = 10.0f;
+        firstWave = true;
+        EnemyNumNow = GameObject.FindGameObjectsWithTag("Enemy").Length;
+        //GameObject.FindGameObjectsWithTag("Enemy").Length
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EnemyNumNow = GameObject.FindGameObjectsWithTag("Enemy").Length;
+        if (firstWave)
+        {
+            TimeCounter -= Time.deltaTime;
+            if (TimeCounter <= 0.0f)
+            {
+                SpawnEnemy();
+                WaveCounter++;
+                TimeCounter = 5.0f;
+                
+                firstWave = false;
+            }
+        }
+        else
+        {
+            if (EnemyNumNow <= 0)
+            {
+                if (WaveCounter < WaveNum)
+                {
+                    TimeCounter -= Time.deltaTime;
+                    if (TimeCounter <= 0.0f)
+                    {
+                        SpawnEnemy();
+                        WaveCounter++;
+                        TimeCounter = 5.0f;
+                        EnemyNumPerWave += 1;
+                        EnemyThresHold += 90;
+                    }
+                }
+                else
+                {
+                    //pass successfully
+                }
+            }
+        }
+
+
+    }
+
+    private void SpawnEnemy()
+    {
+        //Random.seed = System.Guid.NewGuid().GetHashCode();
+        for (int i = 0; i < EnemyNumPerWave; i++)
+        {
+            //tmp_Enemy = Instantiate(FasterEnemy) as GameObject;
+            if (Random.Range(0, 100) > EnemyThresHold)
+            {
+                tmp_Enemy = Instantiate(FasterEnemy) as GameObject;
+            }
+
+            else if (Random.Range(0, 100) <= EnemyThresHold)
+            {
+                tmp_Enemy = Instantiate(HeavyEnemy) as GameObject;
+            }
+
+            int k = Random.Range(0, SpawnPosNum - 1);
+            tmp_Enemy.transform.position = SpawnPointList[k].transform.position;
+
+            tmp_Enemy.SendMessage("SetTarget");
+        }
+
+    }
+}
---
+++ Assets/Scripts/UIWaveCounter.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIWaveCounter : MonoBehaviour
+{
+    public Text CounterText;
+    public Text TimeText;
+    private EnemyGenerator _enemygenerator;
+
+    // Use this for initialization
+    void Start()
+    {
+        //_enemygenerator.WaveCounter
+        _enemygenerator = GameObject.Find("EnemyGenerator").GetComponent<EnemyGenerator>();
+        CounterText.text = string.Format("Wave:{0}\nEnemy Num:{1}", _enemygenerator.WaveCounter, _enemygenerator.EnemyNumNow);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_enemygenerator.EnemyNumNow > 0)
+        {
+            TimeText.enabled = false;
+            CounterText.enabled = true;
+            CounterText.text = string.Format("Wave:{0}\nEnemy Num:{1}", _enemygenerator.WaveCounter, _enemygenerator.EnemyNumNow);
+
+        }
+        else {
+            TimeText.enabled = true;
+            CounterText.enabled = false;
+            TimeText.text = string.Format("Next Wave Remain:{0}", (int)_enemygenerator.TimeCounter);
+
+        }
+    }
+}
